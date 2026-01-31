3308aabfa269f61a27857e0b7bc0015074858a10
dhuff93 <>
2026-01-31 01:54:17 +0000

    Tuple indexing

---
+++ mastering_python_tuples/tuple_indexing/main.py
@@ -0,0 +1,11 @@
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+
+# Write your code here
+first_movie = space_movies[0]
+last_movie = space_movies[-1]
+
+favorite_movies = [space_movies[0], space_movies[-1]]
+
+
+# Testing 
+print("Favorite movies about space:", favorite_movies)
